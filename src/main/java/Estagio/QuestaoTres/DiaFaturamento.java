002008043a8936fdb124b81faab80f4a3d6e583c
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2024-09-02 09:37:20 -0300

    commit

---
+++ src/main/java/Estagio/QuestaoTres/DiaFaturamento.java
@@ -0,0 +1,20 @@
+package Estagio.QuestaoTres;
+
+
+public class DiaFaturamento {
+    private int dia;
+    private double valor;
+
+    public DiaFaturamento(int dia, double valor) {
+        this.dia = dia;
+        this.valor = valor;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
