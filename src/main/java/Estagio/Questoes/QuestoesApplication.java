a39b5d9c2fc05e94d61ed853337fd35ca3273aa9
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2024-09-02 08:00:47 -0300

    commit

---
+++ src/main/java/Estagio/Questoes/QuestoesApplication.java
@@ -0,0 +1,13 @@
+package Estagio.Questoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestoesApplication.class, args);
+	}
+
+}
