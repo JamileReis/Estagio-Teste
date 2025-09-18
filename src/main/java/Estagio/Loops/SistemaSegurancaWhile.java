20600b7b5332ba5e0e2e6287fb945705f34fb4dd
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2025-09-18 10:59:19 -0300

    for training

---
+++ src/main/java/Estagio/Loops/SistemaSegurancaWhile.java
@@ -0,0 +1,38 @@
+package Estagio.Loops;
+
+import java.util.Scanner;
+
+public class SistemaSegurancaWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final String SENHA_CORRETA = "1234";
+        final int MAX_TENTATIVAS = 3;
+        int tentativas = 0;
+        boolean acessoConcedido = false;
+
+        System.out.println("=== SISTEMA BANCÁRIO (WHILE) ===");
+
+        while (tentativas < MAX_TENTATIVAS && !acessoConcedido) {
+            System.out.print("Digite sua senha: ");
+            String senhaDigitada = scanner.nextLine();
+
+            if (senhaDigitada.equals(SENHA_CORRETA)) {
+                acessoConcedido = true;
+                System.out.println("Senha Correta! Acesso concedido!");
+            } else {
+                tentativas++;
+                int tentativasRestantes = MAX_TENTATIVAS - tentativas;
+                if (tentativasRestantes > 0) {
+                    System.out.println("Senha incorreta! Tentativas restantes: " + tentativasRestantes);
+                }
+            }
+        }
+
+        if (!acessoConcedido) {
+            System.out.println("Conta bloqueada! Número máximo de tentativas excedido.");
+            System.out.println("Entre em contato com o suporte para desbloquear sua conta.");
+        }
+
+        scanner.close();
+    }
+}
