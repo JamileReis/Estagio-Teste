becfa43ac86417cf13975e14902635e80f24d062
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2025-10-03 10:06:47 -0300

    atividades de treinamento

---
+++ src/main/java/Estagio/DataHora/Atividade3.java
@@ -0,0 +1,17 @@
+package Estagio.DataHora;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Atividade3 {
+    public static void main(String[] args) {
+        LocalDate dataInicio = LocalDate.now();
+        int prazoDias = 15;
+        LocalDate dataEntrega = dataInicio.plusDays(prazoDias);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String dataFormatada = dataEntrega.format(formatter);
+
+        System.out.println("Data de entrega: " + dataFormatada);
+    }
+}
