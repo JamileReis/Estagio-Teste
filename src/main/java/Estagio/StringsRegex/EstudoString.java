0e9d1f59ab46fb5292fa89e44a93ebbaba6bae37
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2025-10-09 11:00:02 -0300

    manipulação do string

---
+++ src/main/java/Estagio/StringsRegex/EstudoString.java
@@ -0,0 +1,21 @@
+package Estagio.StringsRegex;
+
+public class EstudoString {
+    public static void main(String[] args) {
+        String professor = "Jacqueline Oliveira";
+
+        String disciplina = "Java e Programação Orientada a Objetos";
+
+        String curriculo = ""
+                + "Pós-graduada em Engenharia e "
+                + "Arquitetura de Software; "
+                + "Desenvolvedora backend Java desde 2010; "
+                + "";
+
+        String texto = String.format("Disciplina: %s - %s", disciplina, professor);
+        System.out.println(texto);
+
+        System.out.printf("Nome: %s %nDisciplina: %s", professor, disciplina);
+        System.out.println("\n" + curriculo);
+    }
+}
